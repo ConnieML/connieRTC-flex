7b67e98017d9019e1586a2806956b6ae09a4a7ad
CJB <chris.berno@gmail.com>
2025-05-05 12:22:52 -0400

    Update CRM container to use dynamic profile_url

--- plugin-flex-ts-template-v2/src/feature-library/enhanced-crm-container/index.ts
+++ plugin-flex-ts-template-v2/src/feature-library/enhanced-crm-container/index.ts
@@ -1,9 +1,17 @@
-import { FeatureDefinition } from '../../types/feature-loader';
-import { isFeatureEnabled } from './config';
-// @ts-ignore
-import hooks from './flex-hooks/**/*.*';
+import { FlexPlugin } from '@twilio/flex-plugin';
 
-export const register = (): FeatureDefinition => {
-  if (!isFeatureEnabled()) return {};
-  return { name: 'enhanced-crm-container', hooks: typeof hooks === 'undefined' ? [] : hooks };
-};
+const PLUGIN_NAME = 'EnhancedCRMContainerPlugin';
+
+export default class EnhancedCRMContainerPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  init(flex: typeof import('@twilio/flex-ui'), manager: import('@twilio/flex-ui').Manager) {
+    flex.CRMContainer.defaultProps.uriCallback = (task: import('@twilio/flex-ui').ITask) => {
+      const profileUrl: string = task?.attributes?.profile_url || 'https://v1.connie.plus'; // Fallback to default
+      console.log(`Loading CRM URL: ${profileUrl}`); // Debug log
+      return profileUrl;
+    };
+  }
+}
