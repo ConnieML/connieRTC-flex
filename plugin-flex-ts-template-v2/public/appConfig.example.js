72b66e0213071446310e952ce291ca8d3a3a4634
CJB <chris.berno@gmail.com>
2025-06-04 08:30:37 -0400

    Restore appConfig.example.js with safe placeholder

---
+++ plugin-flex-ts-template-v2/public/appConfig.example.js
@@ -0,0 +1,19 @@
+// Your account sid goes here
+var accountSid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+var appConfig = {
+  pluginService: {
+    enabled: true,
+    url: '/plugins',
+  },
+  ytica: false, // Disable Ytica/Insights to avoid CORS issues
+  logLevel: 'debug', // Set to debug for more detailed logs
+  showSupervisorDesktopView: true,
+  custom_data: {
+    serverless_functions_protocol: 'https',
+    serverless_functions_port: '',
+    serverless_functions_domain: 'connie-profiles-v01-6987.twil.io',
+    common: {},
+    features: {},
+  },
+};
